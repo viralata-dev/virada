be206d0132a3e8193293c4667e03a0999b1a722b
Olú Marques <viralata.dev@gmail.com>
2026-05-22 15:16:29 -0300

    fix: update Header component to use imported fullLogo instead of hardcoded path

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Container, Flex } from "@mantine/core";
+import { fullLogo } from "@public/images";
 import Image from "next/image";
 import { UserMenu } from "../UserMenu";
 
@@ -6,7 +7,7 @@ export const Header = () => {
   return (
     <Container component="header" py="md">
       <Flex align="center" justify="space-between">
-        <Image src="/full-logo.svg" alt="Virada Cultural 2026" width={66} height={39} />
+        <Image src={fullLogo} alt="Virada Cultural 2026" width={66} height={39} />
         <UserMenu />
       </Flex>
     </Container>
