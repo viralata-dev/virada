abd5142915e948081db439a0cff3ac50ee13536e
Olú Marques <viralata.dev@gmail.com>
2026-05-23 15:39:02 -0300

    feat(ActionGlyph): add 'expand' variant to ActionGlyph component

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,24 @@
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
+
+const srcPath = fileURLToPath(new URL("./src", import.meta.url));
+const publicPath = fileURLToPath(new URL("./public", import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": srcPath,
+      "@app": `${srcPath}/app`,
+      "@components": `${srcPath}/app/components`,
+      "@data": `${srcPath}/data`,
+      "@hooks": `${srcPath}/hooks`,
+      "@types": `${srcPath}/types`,
+      "@utils": `${srcPath}/utils`,
+      "@public": publicPath,
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
